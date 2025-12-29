032b0195e00e3e2af30ca7932b31d9da962f77d5
wall-69 <49759454+wall-69@users.noreply.github.com>
2025-12-29 21:36:46 +0100

    my profile update

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="py-5 bg-dark text-white">
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+
+        <div>
+            <h5 class="fw-bold">Pythonškola</h5>
+            <p class="text-white-50">Interaktívna platforma na učenie sa Pythonu.</p>
+        </div>
+
+        <div>
+            <h6 class="fw-bold">Obsah</h6>
+            <ul class="list-unstyled">
+                <li><a href="{{ route('lectures.index') }}" class="text-white-50">Lekcie</a></li>
+                <li><a href="{{ route('exercises.index') }}" class="text-white-50">Cvičenia</a></li>
+                <li><a href="{{ route('quizzes.index') }}" class="text-white-50">Kvízy</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h6 class="fw-bold">Účet</h6>
+            <ul class="list-unstyled">
+                @guest
+                    <li><a href="{{ route('login') }}" class="text-white-50">Prihlásenie</a></li>
+                    <li><a href="{{ route('register') }}" class="text-white-50">Registrácia</a></li>
+                @endguest
+                @auth
+                    <li><a href="{{ auth()->user()->profile_url }}" class="text-white-50">Môj profil</a></li>
+                @endauth
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="text-center text-white-50 mt-4">
+        <span class="fw-bold">© {{ date('Y') }} {{ env('APP_NAME') }}.</span> Všetky práva vyhradené.
+    </div>
+</footer>
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -18,61 +18,51 @@
         <x-alerts />
 
         <header class="sticky-top bg-light">
-            <nav class="navbar navbar-expand-md border-bottom">
-                <div class="container">
-                    <a href="{{ route('index') }}" class="navbar-brand">
-                        <x-logo />
-                    </a>
-                    <button class="ms-auto bg-transparent text-white border-0 d-md-none align-items-center d-flex"
-                        data-bs-toggle="collapse" data-bs-target="#nav">
-                        <i class="bx bx-menu bx-lg"></i>
-                    </button>
-                    <div id="nav" class="collapse navbar-collapse justify-content-center">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a href="{{ route('lectures.index') }}" class="nav-link @active('lectures.index')">Lekcie</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('exercises.index') }}" class="nav-link @active('exercises.index')">Cvičenia</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('quizzes.index') }}" class="nav-link @active('quizzes.index')">Kvízy</a>
-                            </li>
-                        </ul>
+            <header class="bg-brand-purple sticky-top">
+                <nav class="navbar navbar-expand-md shadow">
+                    <div class="container">
+                        <button class="ms-auto bg-transparent text-white border-0 d-md-none align-items-center d-flex"
+                            data-bs-toggle="collapse" data-bs-target="#nav">
+                            <i class="bx bx-menu bx-lg"></i>
+                        </button>
+                        <div id="nav" class="collapse navbar-collapse justify-content-center">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a href="{{ route('lectures.index') }}"
+                                        class="nav-link text-white text-decoration-underline @active('lectures.index')">Lekcie</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('exercises.index') }}"
+                                        class="nav-link text-white text-decoration-underline @active('exercises.index')">Cvičenia</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('quizzes.index') }}"
+                                        class="nav-link text-white text-decoration-underline @active('quizzes.index')">Kvízy</a>
+                                </li>
+                            </ul>
 
-                        <div
-                            class="ms-auto d-flex gap-3 justify-content-start justify-content-md-center align-items-center">
-                            @guest
-                                <a href="{{ route('login') }}" class="btn btn-outline-primary">Prihlásiť sa</a>
-
-                                <a href="{{ route('register') }}" class="btn btn-primary">
-                                    Registrovať sa
-                                </a>
-                            @endguest
-                            @auth
-                                {{-- My profile --}}
-                                <a href="{{ auth()->user()->profile_url }}" class="text-decoration-none">
-                                    Môj profil
-                                </a>
-
-
-                                {{-- Log out --}}
-                                <form action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    @method('POST')
-
-                                    <button type="submit" class="nav-link link-primary">Odhlásiť sa</button>
-                                </form>
-
-                                {{-- Admin panel --}}
-                                @if (auth()->user()->admin)
-                                    <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Admin panel</a>
-                                @endif
-                            @endauth
+                            <div
+                                class="ms-auto d-flex gap-3 justify-content-start justify-content-md-center align-items-center">
+                                @guest
+                                    <a href="{{ route('login') }}" class="btn btn-primary">Prihlásiť sa</a>
+                                    <a href="{{ route('register') }}" class="btn btn-warning">Registrovať sa</a>
+                                @endguest
+                                @auth
+                                    <a href="{{ auth()->user()->profile_url }}" class="text-decoration-none text-white">Môj
+                                        profil</a>
+                                    <form action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button type="submit" class="nav-link text-white">Odhlásiť sa</button>
+                                    </form>
+                                    @if (auth()->user()->admin)
+                                        <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Admin panel</a>
+                                    @endif
+                                @endauth
+                            </div>
                         </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </header>
         </header>
 
         <main class="flex-grow-1 row g-0">
@@ -80,6 +70,8 @@
                 @yield('content')
             </div>
         </main>
+
+        <x-footer />
     </div>
 
     {{-- Completed quizzes & exercises --}}
